85a4b9d3cbe9da7aef9c6f8626ce97e3bd1080eb
pratik519 <prratikpandey@192.168.1.19>
2026-04-18 13:18:53 +0530

    covered till chatbook project

--- oops_proj.py
+++ oops_proj.py
@@ -13,16 +13,69 @@ class chatbook:
                         3.Press 3 to write a post
                         4. Press 4 to write a message to friend 
                         5.Press any key tto end""")
-         
+         user_input = int(input("Enter choice: "))
+
          if user_input ==1:
-             pass
+             self.signup()
          elif user_input ==2:
-             pass
+             self.signin()
          elif user_input ==3:
-             pass
+             self.my_Post()
          elif user_input ==4:
-             pass
+             self.send_message()
          else:
              exit()
 
+
+
+
+    def signup(self):
+        email =input("Enter the email")
+        password=input("eneter the password ")
+        self.user_name=email
+        self.password=password
+        print("you have signed up suceesfully")
+        print("\n")
+        self.menu()
+    def signin(self):
+        if self.user_name=="" and self.password=="":
+            print("First signup by pasin the in the main menu ")
+        else:
+            uname=input("eneter the username")
+            pwd=input("eneter the password")
+
+            if self.user_name==uname and self.password==pwd:
+                print("you have signed i sucessfully ")
+                self.loggedin= True
+            else:
+                print("enter true username and password ")   
+
+    def my_Post(self):
+        if self.loggedin==True:
+            txt=input("enter the message -->") 
+            print("Following content has been posted ")
+        else:
+            print("YOu need to sign in first ")    
+            print("/n")
+            self.menu()
+
+    def send_message(self):
+        if self.loggedin==True:
+            txt=input("Enter the message ")
+            frnd=input("for who ur sending the message ")
+            print(f"YOur message has been sent to{frnd}")
+        else:
+            print("YOu need to sign in first ")    
+            print("/n")
+            self.menu()    
+
 obj=chatbook()
+
+       
+
+
+
+
+
+
+
---
+++ rough.py
@@ -0,0 +1,5 @@
+ls=[1,2,3]
+str="Mlop"
+
+from oops_proj import chatbook
+user1 =chatbook()
